d81405a8a6ab2acd1f1fc0e6897460ca761461f2
saketraja26 <116904929+saketraja26@users.noreply.github.com>
2025-11-11 12:44:48 +0530

    ESLint off

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,8 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = { output: "standalone" };
+const nextConfig: NextConfig = { 
+  output: "standalone",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 export default nextConfig;
